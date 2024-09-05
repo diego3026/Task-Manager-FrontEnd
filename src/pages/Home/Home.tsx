2eff86cfc2277c64f7cf18eaddfb3f839c55e906
diego <diegoonate3026@gmail.com>
2024-09-05 15:11:59 -0500

    empezando proyecto de frontend de devops

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import Header from "../../components/Header/Header";
+
+const Home: React.FC = () => {
+
+  return (
+    <>
+      <Header/>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from 'react-router-dom';
+import { Home } from '../pages';
+
+function RouterProv() {
+    return <Routes>
+              <Route path="/" element={<Home/>}/>
+
+            </Routes>
+  }
+  
+  export default RouterProv; 
